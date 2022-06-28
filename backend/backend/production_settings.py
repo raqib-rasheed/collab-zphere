a1e9568f90670fdcfdcffff5dec56b4550bab6dd
codding_beast <ashishmadhu26@gmail.com>
2022-06-28 16:15:30 +0530

    updates

---
+++ backend/backend/production_settings.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+from decouple import config
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
