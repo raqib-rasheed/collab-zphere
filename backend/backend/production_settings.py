22ff5ef8b40dcc12ecbf59bb3c8745793728fa8f
codding_beast <ashishmadhu26@gmail.com>
2022-06-28 17:21:21 +0530

    added s3 bucket configs

--- backend/backend/production_settings.py
+++ backend/backend/production_settings.py
@@ -4,6 +4,8 @@ from decouple import config
 
 DEBUG = False
 
+INSTALLED_APPS.append('storages')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -14,3 +16,32 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = config('AWS_LOCATION')
+
+# to set file permission to public
+# AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
+
+# to hide access and signature keys in static url
+AWS_QUERYSTRING_AUTH = False
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, '../texa_static'),
+# ]
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+TEMP = os.path.join(BASE_DIR, 'temp')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
