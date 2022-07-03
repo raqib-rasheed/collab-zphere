5a95d48ccb6be9a5df2ef5b344b75ab76c8c3a82
codding_beast <ashishmadhu26@gmail.com>
2022-07-03 13:33:03 +0530

    added node-buttons

--- frontend/src/routes/BotsWorkspace/CustomNode/CustomNode.tsx
+++ frontend/src/routes/BotsWorkspace/CustomNode/CustomNode.tsx
@@ -1,6 +1,6 @@
 import { FC, memo } from "react";
 import { Node, Handle, Position } from "react-flow-renderer";
-import {getIconUrl} from 'helpers/assets/Images'
+import { getIconUrl } from "helpers/assets/Images";
 
 const CustomNode: FC<Node> = ({ data }) => {
     return (
@@ -10,6 +10,22 @@ const CustomNode: FC<Node> = ({ data }) => {
                 {/* if draggable is not set we can drag the icons from flow space   */}
             </div>
             <p>{data.label}</p>
+            <div className="node-buttons">
+                <>
+                    <div className="node-button">
+                        <span className="fas fa-trash"></span>
+                    </div>
+                    <div className="node-button">
+                        <span
+                            className="fas fa-edit"
+                            // onClick={() => {
+                            //     props.data.drawSidebar(props.data.label, props.id);
+                            //     // tour?.show("model")
+                            // }}
+                        ></span>
+                    </div>
+                </>
+            </div>
             <Handle type="source" id="left" position={Position.Left} />
             <Handle type="source" id="right" position={Position.Right} />
         </div>
