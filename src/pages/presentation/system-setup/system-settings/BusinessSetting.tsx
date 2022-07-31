45d7997df80eee97df4ad03b0d955e404afd0054
vaisakh <vaisakhtp133@gmail.com>
2022-07-31 20:18:56 +0530

    system-setup > business setting ui update.

--- src/pages/presentation/system-setup/system-settings/BusinessSetting.tsx
+++ src/pages/presentation/system-setup/system-settings/BusinessSetting.tsx
@@ -1,57 +1,162 @@
-import React, { useState } from 'react';
-import moment from 'moment';
-import { Calendar as DatePicker } from 'react-date-range';
-import SubHeader, { SubHeaderLeft, SubHeaderRight } from '../../../../layout/SubHeader/SubHeader';
-import Icon from '../../../../components/icon/Icon';
-import Button from '../../../../components/bootstrap/Button';
-import Page from '../../../../layout/Page/Page';
-import PageWrapper from '../../../../layout/PageWrapper/PageWrapper';
-import CommonUpcomingEvents from '../../../common/CommonUpcomingEvents';
-import Popovers from '../../../../components/bootstrap/Popovers';
+import React from 'react';
 import useDarkMode from '../../../../hooks/useDarkMode';
+import Card, { CardBody, CardHeader } from '../../../../components/bootstrap/Card';
+import Button from '../../../../components/bootstrap/Button';
+import Icon from '../../../../components/icon/Icon';
+import Input from '../../../../components/bootstrap/forms/Input';
+import Checks from '../../../../components/bootstrap/forms/Checks';
 
-const ListFluidPage = () => {
+const BusinessSetting = () => {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { themeStatus } = useDarkMode();
 
-	const [date, setDate] = useState(new Date());
-
 	return (
-		<PageWrapper title='demo title'>
-			<SubHeader>
-				<SubHeaderLeft>
-					<Icon icon='Info' className='me-2' size='2x' />
-					<span className='text-muted'>
-						You have <Icon icon='TaskAlt' color='success' className='mx-1' size='lg' />{' '}
-						3 approved appointments and{' '}
-						<Icon icon='Alarm' color='warning' className='mx-1' size='lg' /> 4 pending
-						appointments for today.
-					</span>
-				</SubHeaderLeft>
-				<SubHeaderRight>
-					<Popovers
-						desc={
-							<DatePicker
-								onChange={(item) => setDate(item)}
-								date={date}
-								color={process.env.REACT_APP_PRIMARY_COLOR}
+		<>
+			<CardHeader size={'sm'} borderSize={1} className='d-block'>
+				<div className='fw-bold fs-5'>Business Setting</div>
+				<div className='fs-6'>Edit details about your Company</div>
+			</CardHeader>
+			<CardBody>
+				<div className='d-flex justify-content-around'>
+					<Card className='w-25'>
+						<CardHeader size={'sm'} borderSize={1} className='d-block'>
+							<div className='fw-bold fs-6'>Logo Dark</div>
+						</CardHeader>
+						<CardBody className='d-flex flex-column ' style={{ alignItems: 'center' }}>
+							<img
+								src=''
+								alt=''
+								style={{
+									height: '60px',
+									width: '120px',
+									margin: '0 0 10px 0',
+									backgroundColor: '#FEFEFE',
+								}}
 							/>
-						}
-						placement='bottom-end'
-						className='mw-100'
-						trigger='click'>
-						<Button color={themeStatus}>
-							{`${moment(date).startOf('weeks').format('MMM Do')} - ${moment(date)
-								.endOf('weeks')
-								.format('MMM Do')}`}
-						</Button>
-					</Popovers>
-				</SubHeaderRight>
-			</SubHeader>
-			<Page container='fluid'>
-				<CommonUpcomingEvents isFluid />
-			</Page>
-		</PageWrapper>
+
+							<Button color='info'>
+								<label htmlFor='file-upload' className='custom-file-upload'>
+									<Icon icon='Upload' /> Choose file here
+								</label>
+								<input id='file-upload' type='file' />
+							</Button>
+						</CardBody>
+					</Card>
+					<Card className='w-25'>
+						<CardHeader size={'sm'} borderSize={1} className='d-block'>
+							<div className='fw-bold fs-6'>Logo light</div>
+						</CardHeader>
+						<CardBody className='d-flex flex-column ' style={{ alignItems: 'center' }}>
+							<img
+								src=''
+								alt=''
+								style={{
+									height: '60px',
+									width: '120px',
+									margin: '0 0 10px 0',
+									backgroundColor: '#FEFEFE',
+								}}
+							/>
+
+							<Button color='info'>
+								<label htmlFor='file-upload' className='custom-file-upload'>
+									<Icon icon='Upload' /> Choose file here
+								</label>
+								<input id='file-upload' type='file' />
+							</Button>
+						</CardBody>
+					</Card>
+					<Card className='w-25'>
+						<CardHeader size={'sm'} borderSize={1} className='d-block'>
+							<div className='fw-bold fs-6'>Favicon</div>
+						</CardHeader>
+						<CardBody className='d-flex flex-column ' style={{ alignItems: 'center' }}>
+							<img
+								src=''
+								alt=''
+								style={{
+									height: '60px',
+									width: '120px',
+									margin: '0 0 10px 0',
+									backgroundColor: '#FEFEFE',
+								}}
+							/>
+
+							<Button color='info'>
+								<label htmlFor='file-upload' className='custom-file-upload'>
+									<Icon icon='Upload' /> Choose file here
+								</label>
+								<input id='file-upload' type='file' />
+							</Button>
+						</CardBody>
+					</Card>
+				</div>
+				<div className='d-flex'>
+					<div className='w-50'>
+						<label htmlFor='titletext' className='b'>
+							<b>Title Text</b>
+						</label>
+						<Input
+							name='titletext'
+							placeholder='Title text'
+							defaultValue={'ERPGo SaaS'}
+							className='mt-1'
+						/>
+					</div>
+					<div className='d-flex flex-column ms-5'>
+						<b>RTL</b>
+
+						<Checks id='rtl' name='rtll' type='switch' className='mt-3' />
+					</div>
+				</div>
+				<div className='mt-4'>
+					<p className='h5'>Theme Customizer</p>
+					<div className='d-flex mt-3'>
+						<div className='w-25'>
+							<div>
+								<Icon icon='LayoutSidebar' />
+								<b className='ms-1'>Primary color settings</b>
+							</div>
+							<div>----------------------</div>
+							<div></div>
+						</div>
+						<div className='ms-5 w-25'>
+							<div>
+								<Icon icon='LayoutTextWindowReverse' />
+								<b className='ms-1'>Sidebar settings</b>
+							</div>
+							<div>----------------------</div>
+							<div className='d-flex'>
+								<Checks
+									id='rtl'
+									name='rtll'
+									type='switch'
+									style={{ height: '18px', width: '35px' }}
+								/>
+								<div className='ms-2 mt-1'>Transparent layout</div>
+							</div>
+						</div>
+						<div className='ms-5 w-25'>
+							<div>
+								<Icon icon='Sun' />
+								<b className='ms-1'>Layout settings</b>
+							</div>
+							<div>----------------------</div>
+							<div className='d-flex'>
+								<Checks
+									id='rtl'
+									name='rtll'
+									type='switch'
+									style={{ height: '18px', width: '35px' }}
+								/>
+								<div className='ms-2 mt-1'>Dark Layout</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</CardBody>
+		</>
 	);
 };
 
-export default ListFluidPage;
+export default BusinessSetting;
